c0c98ed522cb304f524bcefe6cabd975a4ce5c96
LukesRepositories <lukemtt@hotmail.co.uk>
2026-01-27 20:41:25 +0000

    5x5 grid of containers added to maths puzzle page

--- lib/pages/maths_puzzle.dart
+++ lib/pages/maths_puzzle.dart
@@ -9,10 +9,15 @@ class MathsPuzzle extends StatefulWidget {
 
 class _MathsPuzzleState extends State<MathsPuzzle> {
 
+  Color _boxColour = Colors.white;
+  int _numberA1 = 0;
+  String _numberStringA1 = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal[400],
+
       appBar: AppBar(
         title: const Text("Maths Puzzle"),
         backgroundColor: Colors.teal[800],
@@ -24,11 +29,35 @@ class _MathsPuzzleState extends State<MathsPuzzle> {
           fontSize: 24,
         ),
       ),
+
       body: Center(
         child: Column(
-          children: [],
+          children: List.generate(5, (rowIndex) {
+            return Row(
+              children: List.generate(5, (colIndex) {
+                return Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 2),
+                    borderRadius: BorderRadius.circular(4),
+                    color: _boxColour, // Changes based on game state
+                  ),
+                  child: Center(
+                    child: Text(
+                      _numberStringA1,
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            );
+          }),
         ),
-      ),
+      )
     );
   }
 }
